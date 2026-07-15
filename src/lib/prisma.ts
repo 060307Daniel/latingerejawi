171f48ce5d6f6e3c1f19d9451009a506c81156a9
060307Daniel <daniellantang02@gmail.com>
2026-07-15 10:59:29 +0800

    Add Prisma SSL debug logging

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,6 +1,12 @@
 import fs from "fs";
+import path from "path";
 import { PrismaClient } from "@prisma/client";
 import { PrismaMariaDb } from "@prisma/adapter-mariadb";
+
+const certPath = path.join(process.cwd(), "certs", "ca.pem");
+
+console.log("CA Exists:", fs.existsSync(certPath));
+console.log("CA Path:", certPath);
 
 const adapter = new PrismaMariaDb({
   host: process.env.DATABASE_HOST!,
@@ -9,7 +15,7 @@ const adapter = new PrismaMariaDb({
   password: process.env.DATABASE_PASSWORD!,
   database: process.env.DATABASE_NAME!,
   ssl: {
-    ca: fs.readFileSync("./certs/ca.pem"),
+    ca: fs.readFileSync(certPath, "utf8"),
   },
 });
 
